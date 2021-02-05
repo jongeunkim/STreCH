0832bf1610f6968cf2d41b09343ac2012ce63f0a
Jongeun Kim <kje0510@gmail.com>
2021-02-05 12:41:08 -0600

    Update STreCH.jl

--- src/STreCH.jl
+++ src/STreCH.jl
@@ -1,3 +1,9 @@
+# Copyright (c) <2021>, <Sven Leyffer & Jongeun Kim>
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
 function get_fixed_nodes(ysol, fixlevel)
     if fixlevel < 0
         return nothing
