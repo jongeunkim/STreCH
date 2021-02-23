8ce4e226083264e41611a3fdd9d7af1e6ff71e05
Jongeun Kim <kje0510@gmail.com>
2021-02-23 11:15:15 -0600

    Update util.jl

--- src/util.jl
+++ src/util.jl
@@ -1,8 +1,8 @@
-# Copyright (c) <2021>, <Sven Leyffer & Jongeun Kim>
-# All rights reserved.
+# Copyright © 2021, UChicago Argonne, LLC
+# All Rights Reserved.
 
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. 
+# LICENSE file in the root directory of this source tree.  
 
 function get_leaves(nodes)
     OrderedSet([n for n in nodes if isempty(intersect(Set([2*n, 2*n+1]), nodes))])
